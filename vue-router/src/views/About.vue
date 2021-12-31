94a4d62d6f25626c73c6d975573bd4e58b95adae
VaheAA <vahe.abov@gmail.com>
2021-12-31 17:52:35 +0400

    vue router tutorial

---
+++ vue-router/src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates,
+      perspiciatis deleniti, consequuntur impedit cupiditate laborum accusamus
+      cumque, dolor debitis placeat dicta magnam ullam nisi iure soluta
+      voluptatem quaerat non architecto provident sed quod laudantium magni
+      tempore veniam. Mollitia corrupti soluta quaerat explicabo, vitae impedit?
+      Eaque molestias optio, magni sunt blanditiis veniam! Accusantium ut nihil,
+      fugiat possimus, impedit eveniet minima at dolorum animi molestias
+      perferendis laudantium dolores? Pariatur voluptatum, ipsa ex nesciunt
+      earum enim aperiam mollitia provident fugiat. Ipsum veniam vitae id quis
+      hic odio, qui ut, eum suscipit tempore voluptas, cupiditate repudiandae
+      quasi. Laudantium nam consectetur, impedit suscipit molestias molestiae.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates,
+      perspiciatis deleniti, consequuntur impedit cupiditate laborum accusamus
+      cumque, dolor debitis placeat dicta magnam ullam nisi iure soluta
+      voluptatem quaerat non architecto provident sed quod laudantium magni
+      tempore veniam. Mollitia corrupti soluta quaerat explicabo, vitae impedit?
+      Eaque molestias optio, magni sunt blanditiis veniam! Accusantium ut nihil,
+      fugiat possimus, impedit eveniet minima at dolorum animi molestias
+      perferendis laudantium dolores? Pariatur voluptatum, ipsa ex nesciunt
+      earum enim aperiam mollitia provident fugiat. Ipsum veniam vitae id quis
+      hic odio, qui ut, eum suscipit tempore voluptas, cupiditate repudiandae
+      quasi. Laudantium nam consectetur, impedit suscipit molestias molestiae.
+    </p>
+  </div>
+</template>
---
+++ vue-router/src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      nulla corporis cupiditate, ex a temporibus laborum velit culpa nihil aut,
+      minima quas rerum quis. Illum facere quaerat ab eius fugit, tempora
+      perferendis soluta culpa quia distinctio iusto nostrum similique omnis?
+      Quos nisi distinctio cumque voluptas neque nulla non eos asperiores
+      explicabo pariatur, ut facilis accusamus quis esse veritatis perferendis,
+      itaque autem similique ea saepe. Voluptas hic, repellendus consequatur
+      minima, id iure iusto suscipit ad dolores laboriosam praesentium nisi
+      architecto. Labore asperiores maiores corrupti facere dignissimos saepe
+      nam porro veritatis sapiente! Nostrum quas ipsa dolorem, obcaecati tempore
+      consequuntur dignissimos accusamus eum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      nulla corporis cupiditate, ex a temporibus laborum velit culpa nihil aut,
+      minima quas rerum quis. Illum facere quaerat ab eius fugit, tempora
+      perferendis soluta culpa quia distinctio iusto nostrum similique omnis?
+      Quos nisi distinctio cumque voluptas neque nulla non eos asperiores
+      explicabo pariatur, ut facilis accusamus quis esse veritatis perferendis,
+      itaque autem similique ea saepe. Voluptas hic, repellendus consequatur
+      minima, id iure iusto suscipit ad dolores laboriosam praesentium nisi
+      architecto. Labore asperiores maiores corrupti facere dignissimos saepe
+      nam porro veritatis sapiente! Nostrum quas ipsa dolorem, obcaecati tempore
+      consequuntur dignissimos accusamus eum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      nulla corporis cupiditate, ex a temporibus laborum velit culpa nihil aut,
+      minima quas rerum quis. Illum facere quaerat ab eius fugit, tempora
+      perferendis soluta culpa quia distinctio iusto nostrum similique omnis?
+      Quos nisi distinctio cumque voluptas neque nulla non eos asperiores
+      explicabo pariatur, ut facilis accusamus quis esse veritatis perferendis,
+      itaque autem similique ea saepe. Voluptas hic, repellendus consequatur
+      minima, id iure iusto suscipit ad dolores laboriosam praesentium nisi
+      architecto. Labore asperiores maiores corrupti facere dignissimos saepe
+      nam porro veritatis sapiente! Nostrum quas ipsa dolorem, obcaecati tempore
+      consequuntur dignissimos accusamus eum.
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
