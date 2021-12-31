f5dd025edbe24dc9832be05905ea9d8a330164b7
VaheAA <vahe.abov@gmail.com>
2021-12-31 21:35:43 +0400

    project planning app done

---
+++ vue-planning-app/src/components/FilterNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="filter-nav">
+    <button @click="updateFilter('all')" :class="{ active: current === 'all' }">
+      See All Projects
+    </button>
+    <button
+      @click="updateFilter('completed')"
+      :class="{ active: current === 'completed' }"
+    >
+      See Completed Projects
+    </button>
+    <button
+      @click="updateFilter('ongoing')"
+      :class="{ active: current === 'ongoing' }"
+    >
+      See Ongoing Projeccts
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    current: String,
+  },
+  methods: {
+    updateFilter(by) {
+      this.$emit("filterChange", by);
+    },
+  },
+};
+</script>
+
+<style>
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+  color: #555;
+}
+</style>
