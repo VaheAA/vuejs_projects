ac7ae1fb285a1134901672e59749f9012196358e
VaheAA <vahe.abov@gmail.com>
2021-12-31 18:28:14 +0400

    jobs

--- vue-router/src/views/Jobs/JobDetails.vue
+++ vue-router/src/views/Jobs/JobDetails.vue
@@ -1,6 +1,11 @@
 <template>
-  <h1>Job details page</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading....</p>
+  </div>
 </template>
 
 <script>
@@ -8,11 +13,17 @@ export default {
   props: {
     id: String,
   },
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //     };
-  //   },
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
