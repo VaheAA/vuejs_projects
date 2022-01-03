4422191b987e67c0134ec21a699a29966978338c
VaheAA <vahe.abov@gmail.com>
2022-01-04 00:52:46 +0400

    new

---
+++ vue-cli-shopping-cart/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="splash-container">
+    <div class="splash">
+      <h1>Splendid Food</h1>
+    </div>
+  </div>
+
+  <main class="wrapper">
+    <h2>Recommended</h2>
+    <div class="recommended">
+      <ProductCatd
+        :product="product"
+        :addToCart="addToCart"
+        v-for="(product, index) in inventory.slice(0, 3)"
+        :index="index"
+        :key="product.id"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+import ProductCatd from '../components/ProductCatd.vue';
+export default {
+  name: 'Home',
+  props: ['inventory', 'addToCart'],
+  components: {ProductCatd}
+};
+</script>
---
+++ vue-cli-shopping-cart/src/views/Products.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+
+    <div class="card-container">
+      <ProductCatd
+        :product="product"
+        :addToCart="addToCart"
+        v-for="(product, index) in inventory.slice(0, 6)"
+        :index="index"
+        :key="product.id"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+import ProductCatd from '../components/ProductCatd.vue';
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {ProductCatd}
+};
+</script>
+
+<style></style>
